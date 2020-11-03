ec1233da6b944d4a96bc0956b2b56d5a7969910f
yeji <owjdo12@naver.com>
2020-11-03 17:38:33 +0900

    혼자놀기

--- src/hellojava/hellojava.java
+++ src/hellojava/hellojava.java
@@ -3,8 +3,8 @@ package hellojava;
 public class hellojava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello.java");
+		System.out.println("혼자놀기");
+		System.out.println("에ㅔㅔ");
 	}
 
 }
