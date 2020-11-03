27cc9ba024bd9ca28a93343fdd3af1d6978c44b9
Hyung uk <shguddnr2@Naver.com>
2020-11-03 17:43:34 +0900

    scanner1

---
+++ src/scanner1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class scanner1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("정수를 입력 하세요 : ");
+		
+		int a ;
+		a=s.nextInt();
+		System.out.printf("%d",a);
+		
+	}
+
+}
