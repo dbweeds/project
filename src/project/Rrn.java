484b86075ea2bb031878d2cf90fae96bb281437e
kimjiho <cafe3ye@naver.com>
2020-11-03 17:07:45 +0900

    시작

---
+++ src/project/Rrn.java
@@ -0,0 +1,12 @@
+package project;
+
+public class Rrn {
+	String rrn;
+	Rrn(String rrn){
+		this.rrn = rrn;
+	}
+	
+	public void rrnException(String rrn) {
+		if(rrn.length() == 13)
+	}
+}
---
+++ src/project/Test.java
@@ -0,0 +1,15 @@
+package project;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String rrn;
+		
+		System.out.print("주민등록번호를 입력하세요 : ");
+		rrn = s.nextLine();
+	}
+
+}
